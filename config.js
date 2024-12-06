98b680956db573745f514e994ca4dc48adfe3038
Amirbot123 <whatsappbot549@gmail.com>
2024-12-06 21:25:23 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "b2A31DqK#JHOPFFClJTf2HLZYI4UxYWhTi4Cn6cKw8L7ywydebLw", //
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
